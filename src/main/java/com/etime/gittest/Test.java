1f9776bf7e09da353df57f23065873e3f1deac3a
haihaohm <602833704@qq.com>
2021-07-25 00:31:31 +0800

    first commit

---
+++ src/main/java/com/etime/gittest/Test.java
@@ -0,0 +1,15 @@
+package com.etime.gittest;
+
+/**
+ * @Package: com.etime.gittest
+ * @ClassName: Test
+ * @Author: lyh
+ * @CreateTime: 2021/7/25 0:25
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
